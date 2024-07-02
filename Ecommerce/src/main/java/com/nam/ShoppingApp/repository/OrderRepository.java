0cd53117b6c8e7138523688fbea9239337f42015
LTNam <namvay0403@gmail.com>
2024-07-02 20:32:44 +0700

    fix: don't save data cartItems into table order

--- Ecommerce/src/main/java/com/nam/ShoppingApp/repository/OrderRepository.java
+++ Ecommerce/src/main/java/com/nam/ShoppingApp/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package com.nam.ShoppingApp.repository;
 import com.nam.ShoppingApp.entity.Order;
 import com.nam.ShoppingApp.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
 import java.util.List;
@@ -10,17 +11,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Optional<Order>  findByUserIdAndOrderStatus(Long userId, OrderStatus orderStatus);
 
-    List<Order> findAllByOrderStatusIn(List<OrderStatus> orderStatuses);
+  @Query("SELECT o FROM Order o WHERE o.user.id = ?1 AND o.orderStatus = ?2")
+  Optional<Order> findByUserIdAndOrderStatus(Long userId, OrderStatus orderStatus);
 
-    List<Order> findByUserIdAndOrderStatusIn(Long userId, List<OrderStatus> orderStatus);
+  List<Order> findAllByOrderStatusIn(List<OrderStatus> orderStatuses);
 
-    Optional<Order> findByTrackingId(UUID trackingId);
+  List<Order> findByUserIdAndOrderStatusIn(Long userId, List<OrderStatus> orderStatus);
 
-    List<Order> findByDateBetweenAndOrderStatus(Date startOfMonth, Date endOfMonth, OrderStatus orderStatus);
+  Optional<Order> findByTrackingId(UUID trackingId);
 
-    Long countByOrderStatus(OrderStatus orderStatus);
+  List<Order> findByDateBetweenAndOrderStatus(
+      Date startOfMonth, Date endOfMonth, OrderStatus orderStatus);
 
-    Optional<Order> findByIdAndUserId(Long orderId, Long userId);
+  Long countByOrderStatus(OrderStatus orderStatus);
+
+  Optional<Order> findByIdAndUserId(Long orderId, Long userId);
 }
--- Ecommerce/src/main/java/com/nam/ShoppingApp/services/customer/review/ReviewServiceImpl.java
+++ Ecommerce/src/main/java/com/nam/ShoppingApp/services/customer/review/ReviewServiceImpl.java
@@ -4,10 +4,14 @@ import com.nam.ShoppingApp.dto.OrderedProductResponseDto;
 import com.nam.ShoppingApp.dto.ProductDto;
 import com.nam.ShoppingApp.dto.ReviewDto;
 import com.nam.ShoppingApp.entity.*;
+import com.nam.ShoppingApp.exception.AppException;
+import com.nam.ShoppingApp.exception.ErrorCode;
 import com.nam.ShoppingApp.repository.OrderRepository;
 import com.nam.ShoppingApp.repository.ProductRepository;
 import com.nam.ShoppingApp.repository.ReviewRepository;
 import com.nam.ShoppingApp.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,13 +23,18 @@ import java.util.Optional;
 @Service
 public class ReviewServiceImpl implements  ReviewService{
 
+    private static final Logger log = LoggerFactory.getLogger(ReviewServiceImpl.class);
     @Autowired private OrderRepository orderRepository;
     @Autowired private ProductRepository productRepository;
     @Autowired private UserRepository userRepository;
     @Autowired private ReviewRepository reviewRepository;
 
     public OrderedProductResponseDto getOrderedProductResponseDto(Long orderId){
-        Optional<Order> optionalOrder = Optional.ofNullable(orderRepository.findById(orderId).orElseThrow(() -> new RuntimeException("Order not found")));
+    Optional<Order> optionalOrder =
+        Optional.ofNullable(
+            orderRepository
+                .findById(orderId)
+                .orElseThrow(() -> new AppException(ErrorCode.ORDER_NOT_FOUND)));
         OrderedProductResponseDto orderedProductResponseDto = new OrderedProductResponseDto();
 
         if(optionalOrder.isPresent()){
@@ -43,6 +52,8 @@ public class ReviewServiceImpl implements  ReviewService{
                 productDto.setByteImg(cartItem.getProduct().getImg());
                 productDtoList.add(productDto);
             }
+
+      System.out.println("size" + productDtoList.size());
             orderedProductResponseDto.setProductDtoList(productDtoList);
         }
         return orderedProductResponseDto;
